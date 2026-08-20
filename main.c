c83163f51dc4ac574f83de5c9fdfb2c403a2c2ee
Gabriel Gabrigna Queiroz <gabrignaa@gmail.com>
2026-08-20 20:42:04 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,45 @@
 #include <stdio.h>
 
-void main()
-{
-    printf("Bem vindo!");
+/*
+int main(){
+    int voto = 0;
+
+    printf("Escolha em quem votar:\n1- Ana\n2- Bob\nDigite seu voto: ");
+    scanf("%d", &voto);
+
+    switch(voto)
+    {
+        case 1: printf("\nO candidato escolhido é a Ana"); break;
+        case 2: printf("\nO candidato escolhido é o Bob"); break;
+        default: printf("\nOpção inválida!");
+    }
+
+    printf("Fim da votação");
+
+    return 0;
+
 }
+*/
+
+int main(){
+    int voto = 0;
+
+    printf("Escolha em quem votar:\n1- Ana\n2- Bob\nDigite seu voto: ");
+    scanf("%d", &voto);
+
+    if(voto == 1){
+        printf("\nO candidato escolhido é a Ana");
+    }
+    else if(voto == 2){
+        printf("\nO candidato escolhido é a Bob");
+    }
+    else{
+        printf("\nVoto nulo!");
+    }
+
+    printf("\nFim da votação");
+
+    return 0;
+
+}
+
