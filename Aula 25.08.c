240094a68b8e19f47e527cf7710e6e83837f4480
Gabriel Gabrigna Queiroz <gabrignaa@gmail.com>
2026-08-25 21:44:31 -0300

    Update Aula 25.08.c

--- Aula 25.08.c
+++ Aula 25.08.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+/*
+
 int main()
 {
     int num = 0;
@@ -14,6 +16,32 @@ int main()
         printf("Numero: %d \n", num);
         num = num + 1;
     }
+
+    return 0;
+}
+
+*/
+
+int main()
+{
+    float nota1 = 0;
+    float nota2 = 0;
+    int pergunta = 2;
+    int num = 1;
+
+    while(pergunta == 2){
+        printf("\nDigite a primeira nota do %d Aluno: ", num);
+        scanf("%f", &nota1);
+        printf("Digite a segunda nota do %d Aluno: ", num);
+        scanf("%f", &nota2);
+        float media = (nota1 + nota2) / 2;
+        printf("\nA media do aluno %d é: %.2f\n\n", num, media);
+        num = num + 1;
+        printf("Digite 1 para parar de conferir as notas e 2 para continuar conferindo: ");
+        scanf("%d", &pergunta);
+    }
+
+    printf("\nFim do Programa!");
 
     return 0;
 }
