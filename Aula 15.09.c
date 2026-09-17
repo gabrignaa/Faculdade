6c9ee249868b83fd0297c019b029e3045998cbb7
Gabriel Gabrigna Queiroz <gabrignaa@gmail.com>
2026-09-17 00:10:25 -0300

    Update Aula 15.09.c

--- Aula 15.09.c
+++ Aula 15.09.c
@@ -55,46 +55,89 @@ int main(){
     int aluno = 0;
     int QtdAlunos = 0;
 
-
     printf("BEM VINDO AO SISTEMA DE NOTAS\n");
 
-    //CAPTURA E GUARDA A QUANTIDADE DE ALINOS DA SALA DIGITADA PELO USUAIRO
+    //CAPTURA E GUARDA A QUANTIDADE DE ALUNOS DA SALA
     printf("Digite a quantidade de alunos da sala: ");
     scanf("%d", &QtdAlunos);
 
-    //CRIAS OS VETORES/MATRIZ COM A QTD DE ALUNOS
+    //CRIA OS VETORES/MATRIZ COM A QTD DE ALUNOS
     float notas[QtdAlunos][3];
     int rgm[QtdAlunos];
 
     //PARA CAPTURAR O RGM E NOTAS A1 E A2 DOS ALUNOS
-    for(cont=0; cont < QtdAlunos; cont++){
-        printf("Digite o RGM do %do do aluno: ", numAluno); //vai de 1 ate a qtd de alunos
-        scanf("%d", &rgm[cont]); //vai de 0 ate a qtd de alunos
+    for(cont = 0; cont < QtdAlunos; cont++){
+
+        printf("Digite o RGM do %do aluno: ", numAluno);
+        scanf("%d", &rgm[cont]);
 
         notas[cont][1] = -1;
         notas[cont][2] = -1;
 
+        //CAPTURA A NOTA 1
         while(notas[cont][1] < 0){
             printf("Digite a nota 1 do %do aluno: ", numAluno);
             scanf("%f", &notas[cont][1]);
+
+            if(notas[cont][1] > 10){
+                notas[cont][1] = -1;
+            }
         }
 
+        //CAPTURA A NOTA 2
         while(notas[cont][2] < 0){
             printf("Digite a nota 2 do %do aluno: ", numAluno);
             scanf("%f", &notas[cont][2]);
+
+            if(notas[cont][2] > 10){
+                notas[cont][2] = -1;
+            }
         }
 
         numAluno += 1;
     }
 
     //PASSA POR TODOS OS ALUNOS CALCULANDO A MEDIA = A1 + A2
-    for(aluno=0; aluno < QtdAlunos; aluno++){
+    for(aluno = 0; aluno < QtdAlunos; aluno++){
         notas[aluno][0] = notas[aluno][1] + notas[aluno][2];
     }
 
+    //AVALIAÇÃO SUBSTITUTIVA
+    for(aluno = 0; aluno < QtdAlunos; aluno++){
+
+        if(notas[aluno][0] < 12){
+
+            float notaSubstitutiva = -1;
+
+            printf("\nAluno RGM %d precisa fazer a substitutiva.\n", rgm[aluno]);
+
+            //CAPTURA A NOTA DA SUBSTITUTIVA
+            while(notaSubstitutiva < 0){
+                printf("Digite a nota da substitutiva: ");
+                scanf("%f", &notaSubstitutiva);
+
+                if(notaSubstitutiva > 10){
+                    notaSubstitutiva = -1;
+                }
+            }
+
+            //SUBSTITUI A MENOR NOTA
+            if(notas[aluno][1] < notas[aluno][2]){
+                notas[aluno][1] = notaSubstitutiva;
+            }
+            else{
+                notas[aluno][2] = notaSubstitutiva;
+            }
+
+            //CALCULA NOVAMENTE A MEDIA
+            notas[aluno][0] = notas[aluno][1] + notas[aluno][2];
+        }
+    }
+
     //MOSTRAR TODAS AS NOTAS ALUNO POR ALUNO
-    for(aluno=0; aluno < QtdAlunos; aluno++){
-        printf("Notas do Aluno RGM %d\n", rgm[aluno]);
+    for(aluno = 0; aluno < QtdAlunos; aluno++){
+
+        printf("\nNotas do Aluno RGM %d\n", rgm[aluno]);
         printf("==================\n");
         printf("Nota A1: %0.1f\n", notas[aluno][1]);
         printf("Nota A2: %0.1f\n", notas[aluno][2]);
@@ -102,17 +145,17 @@ int main(){
     }
 
     //MOSTRA OS ALUNOS QUE FORAM APROVADOS OU REPROVADOS
-    for(aluno=0; aluno < QtdAlunos; aluno++){
+    for(aluno = 0; aluno < QtdAlunos; aluno++){
+
         if(notas[aluno][0] >= 12){
             printf("O aluno %d foi aprovado!\n", numAluno2);
         }
         else{
             printf("O aluno %d foi reprovado!\n", numAluno2);
         }
+
         numAluno2 += 1;
     }
 
-
     return 0;
 }
-
